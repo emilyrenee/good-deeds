ec86466f4a1b5be57f23deec6239282e22a7cc84
emily renee <hagoodem@gmail.com>
2017-06-30 15:48:31 -0400

    user authentication implemented

--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,23 @@
+'use strict'
+
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+//gives monoose information about the document we want to store
 var UserSchema = new mongoose.Schema({
-    email: {
+    firstName: {
       type: String,
-      unique: true,
       required: true,
       trim: true
     },
-    name: {
+    lastName: {
       type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
       required: true,
       trim: true
     },
@@ -15,21 +25,66 @@ var UserSchema = new mongoose.Schema({
       type: String,
       required: true
     },
+    //need to use check-box(s) option as an array
     interests: {
       type: Array,
-      required: true,
+      required: false,
       trim: true
     },
+    //need to use check-box option as an array
     skills: {
       type: Array,
-      required: true,
+      required: false,
       trim: true
     },
     extras: {
       type: String,
-      required: true,
+      required: false,
       trim: true
     },
 });
-var User = mongoose.model('User', UserSchema);
+
+
+//create the authenticate method 
+// authenticate input against database documents
+//statics allows us to apply methods directly to the model
+UserSchema.statics.authenticate = function(email, password, callback) {
+  //callback added in route
+  //find doc with user's email
+  User.findOne({ email: email })
+    //preform search and process results
+    .exec(function (error, user) {
+      if (error) {
+        return callback(error);
+      } else if ( !user ) {
+        var err = new Error('user not found.');
+        err.status = 401;
+        return callback(error);
+      }
+      //compare password input with users hashed password
+      bcrypt.compare(password, user.password, function(error, result) {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
+        }
+      })
+    });
+}
+
+//hash password before user record is stored
+UserSchema.pre('save', function(next) {
+  //user object created with user data
+  var user = this;
+  bcrypt.hash(user.password, 10, function(err, hash) {
+    //callback replace plain text password with hashed one
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
+});
+
+const User = mongoose.model('User', UserSchema);
 module.exports = User;
--- src/router.js
+++ src/router.js
@@ -1,35 +1,152 @@
 'use strict';
+const express = require('express');
+const router = express.Router();
+var User = require('./models/user');
 
-const express = require('express')
-const profile = require('../mock/example.json')
-const router = express.Router()
+
+//Volunteer user profile
+router.get('/profile', function (req, res, next) {
+	if (! req.session.userId ) {
+		var err = new Error("You are not authorized to view this page.");
+		err.status = 403;
+		return next(err);
+	}
+	//capture user id
+	console.log('here');
+	User.findById(req.session.userId);   
+	   exec(function (error, user) {
+		   if (error) {
+	          return next(error);
+	        } else {
+	          console.log('User redirected: ' + req.params.id);
+			  res.sendFile('/profile.html', {root: __dirname});
+			  res.json()
+	          return res.json(user.firstName);
+	        }
+    });  
+});	   
 
 
 //Volunteers sign-up
 router.post('/signup', function(req, res, next) {
-	// log data sent
-	console.log(req.body);
+	//require fields
+	if (req.body.firstName &&
+    req.body.firstName &&
+    req.body.email &&
+    req.body.password &&
+    req.body.confirmPassword) {
+		// log data
+		console.log(req.body);
+		console.log('data provided'); 
 
-	// save data sent
+	    // confirm user typed same password twice
+	    if (req.body.password !== req.body.confirmPassword) {
+	        var err = new Error('Passwords do not match.');
+	        err.status = 400;
+	        return next(err);
+	    }
 
-	//display the data
-	return res.send("Thanks for signing up!")
-})
+	    // create object and new document for db
+        var userData = {
+        	firstName: req.body.firstName,
+        	lastName: req.body.lastName,
+        	email: req.body.email,
+        	password: req.body.password
+      	};
+
+     	// use schema's `create` method to insert document into Mongo
+      	User.create(userData, function (error, user) {
+        	if (error) {
+          	return next(error);
+        	} else {
+          	req.params.userId = user._id;
+          	console.log('user created' + user._id);
+          	return res.redirect('/profile');
+        	}
+      	});
+
+    } else {
+      //user didn't fill out form correctly	
+      var err = new Error('All fields required.');
+      err.status = 400;
+      return next(err);
+    } 
+
+});
+
 
 //Volunteer users update profile
-router.put('/signup', function(req, res) {
+router.put('/profile', function(req, res) {
+	var id = req.params._id;
+	var user = req.body;
+	if (user && user._id !== id) {
+	    return res.status(500).json({err: "Ids don't match!"});
+	}
+
+    User.findByIdAndUpdate(id, userData, function (err, user) {
+		if (err) {
+			return res.status(500).json({err: err.message});
+		}
+  		// req.session.userId = user._id;
+  		console.log('user updated');
+  		res.json({userData: userData, message: "User Updated"});
+  //redirect to profile
+  // return res.redirect('/profile');
+  // console.log('user redirected');
+});
 	//log data sent
 	console.log(req.body);
 
 	// save data sent
 
 	res.json({
-		response: "A volunteer sent me a PUT request to /signup"
+		response: "A volunteer sent me a PUT request to /profile"
 	})
 })
 
+router.get('/signin', function(req, res, next) {
+	res.sendFile('/signin.html', {root: __dirname});
+});
+
 //Volunteer users to sign-in
+router.post('/signin', function(req, res, next) {
+	if (req.body.email && req.body.password) {
+		//call the method we created
+		User.authenticate(req.body.email, req.body.password, function (error, user) {
+			//bad login
+			if (error || !user) {
+				var err = new Error('Wrong email or password.');
+				err.status = 401;
+				return next(err);
+			} else {
+				//get user._id back from authenticate function
+				//user is our document
+				req.session.userId = user._id;
+				return res.redirect('/profile');
+			}
+		});
+
+	} else {
+		var err = new Error('Email and password are required.');
+		err.status = 401;
+		return next(err);
+	}
+});
 	// POST /sign-in
 
+	//redirect to /profile
 
-module.exports = router
+//list all profiles
+// router.get('/profiles', function(req, res, next){
+// 	//list all profiles
+// 	mongoose.model('User').find({}, function(err, users) {
+// 		if (err) {
+//       		console.log(err);
+//       		return res.status(500).json(err);
+//     }
+//     res.send(users);
+// });
+// });	
+
+
+module.exports = router;
